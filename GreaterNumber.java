f361a8720ef85eba0599579149d955dc2ba404ad
Mayank Vaid <mayankvaid21@gmail.com>
2023-02-27 14:29:02 +0530

    First Commit: Adding File: GreaterNumber.java

---
+++ GreaterNumber.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class GreaterNumber {
+
+    public static void main(String args[]) {
+
+        int number1, number2;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter 1st Number");
+        number1 = scan.nextInt();
+
+        System.out.println("Enter 2st Number");
+        number2 = scan.nextInt();
+
+        if(number1>number2){
+            System.out.println("1st Number: " + number1 + " is greater than the 2nd Number: " + number2);
+        } else if(number2>number1) {
+            System.out.println("2nd Number: " + number2 + " is greater than the 1st Number: " + number1);
+        } else {
+            System.out.println("Both numbers are equal");
+        }
+
+        scan.close();
+
+    }
+
+}
